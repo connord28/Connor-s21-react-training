23ea70bdd6c2288c0ff8fbce23990555db6001e6
Connor Daly <connord2838@gmail.com>
2021-04-22 13:25:28 -0700

    second assignment first draft

---
+++ app2/app2/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function Foods(props) {
+  const [number, setNumber] = useState(0);
+  const [itemNum, setItemNum] = useState(1);
+  var message = [];
+  for(var i = 0; i < itemNum; i++)
+  {
+    message = [...message, <div>{props.name} 
+    <br/> 
+    {number} Likes 
+    <br/> 
+    <button onClick={()=>setNumber(number+1)}>Like</button> 
+    <br/> 
+    <button onClick={()=>setItemNum(itemNum+1)}>duplicate</button> 
+    <br/> 
+    <button onClick={()=>setItemNum(itemNum-1)}>remove</button> 
+    </div>];
+    console.log(message);
+  }
+  return message;
+}
+
+function App() {
+  const foods = {
+    "Pizza": 0,
+    "Burrito": 0,
+    "Burger": 0
+  }
+  return (
+    <div className="App">
+    <header className="App-header">
+    {
+      Object.entries(foods).map(([key, _]) => <Foods name={key}/>)
+    }
+    </header>
+    </div>
+  );
+}
+
+export default App;
