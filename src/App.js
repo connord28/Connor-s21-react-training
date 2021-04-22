41e053ee9cd91e1d30cbc17e1bfe20173c50b3d5
Connor Daly <connord2838@gmail.com>
2021-04-22 13:27:57 -0700

    changed app from assignment 1 to assignment 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <p>
-          "favorite snack is" {favoriteSnack()}
-        </p>
-    </div>
-  );
+function Foods(props) {
+  const [number, setNumber] = useState(0);
+  const [itemNum, setItemNum] = useState(1);
+  var message = [];
+  for(var i = 0; i < itemNum; i++)
+  {
+    message = [...message, <div>{props.name} 
+    <br/> 
+    {number} Likes 
+    <br/> 
+    <button onClick={()=>setNumber(number+1)}>Like</button> 
+    <br/> 
+    <button onClick={()=>setItemNum(itemNum+1)}>duplicate</button> 
+    <br/> 
+    <button onClick={()=>setItemNum(itemNum-1)}>remove</button> 
+    </div>];
+    console.log(message);
+  }
+  return message;
 }
 
-function favoriteSnack() {
-  return "chips";
+function App() {
+  const foods = {
+    "Pizza": 0,
+    "Burrito": 0,
+    "Burger": 0
+  }
+  return (
+    <div className="App">
+    <header className="App-header">
+    {
+      Object.entries(foods).map(([key, _]) => <Foods name={key}/>)
+    }
+    </header>
+    </div>
+  );
 }
 
 export default App;
